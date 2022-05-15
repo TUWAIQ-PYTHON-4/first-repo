67e2571e083016e8fa2e534050dcb18e8e35b3d6
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-15 23:26:23 +0300

    added file

---
+++ main.py
@@ -0,0 +1,17 @@
+my_num:int = 90 
+
+# naming convention : snake_case   like pascal case and other
+
+#assign one value to multiple var
+var1 , var2 ="hello"
+
+#assign multiple values to multiple var
+var11 , var22 ="value1","value2"
+print(var11)
+
+#convert from one value to the other
+print(str(var1))
+
+#check the type of a varible
+print(type(var1))
+
