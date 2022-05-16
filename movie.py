01ac2756f0af16b99e1807ed9a7f5f5d20914b12
ahmed <a-5amd@hotmail.com>
2022-05-16 10:13:52 +0300

    first lab

---
+++ movie.py
@@ -0,0 +1,16 @@
+movie_1 = "nonstop" 
+rate_movie1 = 3
+score_movie = 72.65
+if rate_movie1 >= 4 and score_movie> 80:
+    print("Highly recommended")
+
+
+elif rate_movie1 >= 3 and score_movie> 70:
+    print("I recommended it . It is good")
+
+elif rate_movie1 >= 2 and score_movie> 60:
+    print("You should check it out!")
+
+
+else :
+     print("Don't watch it. It is a waste of time")
