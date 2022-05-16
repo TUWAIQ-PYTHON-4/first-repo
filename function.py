9741a6a879a15b63f8ecd3585dbf7b8aaeea8b01
ahmed <a-5amd@hotmail.com>
2022-05-16 16:21:38 +0300

    function

---
+++ function.py
@@ -0,0 +1,29 @@
+def function_lab( number:int):
+    
+    for number1 in range(number, 0,-1 ):
+         for number2 in range (number1,0,-1):
+              print(number2 ,end=" ")
+         print(" ")
+        
+
+function_lab(6)
+
+
+    
+        
+
+        
+
+
+
+
+function_lab(4)
+
+
+        
+
+
+
+        
+ 
+    
